fff41e40559db8b5aca95ff4bcd6fd611730e3ee
themandysingh <mandy98@outlook.com>
2018-07-06 09:44:01 +0530

    java script functionality added

---
+++ FlexGallery.js
@@ -0,0 +1,18 @@
+(function() {
+    window.onload = function() { 
+        const panels = document.querySelectorAll(".panel");
+        panels.forEach(panel => panel.addEventListener("click", toggleOpen));
+        panels.forEach(panel => panel.addEventListener("transitionend", toggleActive));
+            
+    };   
+
+    function toggleOpen() {
+        this.classList.toggle("open");
+    }
+
+    function toggleActive(e) {
+        if (e.propertyName.includes("flex")) {
+            this.classList.toggle("open-active");
+        }
+    }
+})();
